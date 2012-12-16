7ae6db8f065bdaf65c98438ae8244206edcd2e4b
José Javier Martinez Domene <josejamart@gmail.com>
2012-12-16 19:19:04 +0100

    I Started creating an action to launch a wizard to generate steps code.

---
+++ org.jbehave.eclipse/src/org/jbehave/eclipse/actions/CreateGenerateStepsWizardAction.java
@@ -0,0 +1,61 @@
+package org.jbehave.eclipse.actions;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IPartListener;
+import org.eclipse.ui.IWorkbenchPart;
+
+public class CreateGenerateStepsWizardAction implements IObjectActionDelegate, IPartListener {
+
+	@Override
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void partActivated(IWorkbenchPart part) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void partBroughtToTop(IWorkbenchPart part) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void partClosed(IWorkbenchPart part) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void partDeactivated(IWorkbenchPart part) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void partOpened(IWorkbenchPart part) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
---
+++ org.jbehave.eclipse/src/org/jbehave/eclipse/wizards/GenerateStepsWizard.java
@@ -0,0 +1,71 @@
+package org.jbehave.eclipse.wizards;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.core.runtime.*;
+import org.eclipse.jface.operation.*;
+import java.lang.reflect.InvocationTargetException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.CoreException;
+import java.io.*;
+import org.eclipse.ui.*;
+import org.eclipse.ui.ide.IDE;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GenerateStepsWizard extends Wizard implements INewWizard {
+	private static Logger log = LoggerFactory.getLogger(GenerateStepsWizard.class);
+	
+	private NewStoryWizardPage page;
+	private IStructuredSelection selection;
+	private IWorkbench workbench;
+
+	public GenerateStepsWizard() {
+		super();
+		setWindowTitle(WizardsMessages.NewStoryWizard_0);
+	}
+	
+	public void addPages() {
+		page = new NewStoryWizardPage(selection);
+		addPage(page);
+	}
+	
+	private void openEditor(final IFile file){
+		if(file != null){
+			getShell().getDisplay().asyncExec(new Runnable(){
+				public void run(){
+					try{
+						IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+						IDE.openEditor(page, file,true);
+					}catch( PartInitException e){
+						log.debug(e.getMessage(), e);
+					}
+				}
+			});
+		}
+	}
+
+	public boolean performFinish() {
+		boolean performedOK = false;
+		
+		IFile file = page.createNewFile();
+		if(file != null)
+		{
+			// open the file in editor
+			openEditor(file);
+			
+			// everything is fine
+			performedOK = true;
+		}
+		return performedOK;
+	}
+	
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		this.selection = selection;
+		this.workbench = workbench;
+	}
+}
