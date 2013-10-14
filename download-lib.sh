e77f8c125527b72945e60a076269fb7f81f254cd
Mauro Talevi <mauro.talevi@aquilonia.org>
2013-10-14 16:02:03 +0200

    Updated plugin build to use the same version of core as the Maven build.

--- download-lib.sh
+++ download-lib.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm org.jbehave.eclipse/lib/*.jar
+
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.4:copy-dependencies -forg.jbehave.eclipse/pom.xml -Ddest=org.jbehave.eclipse/lib
 
